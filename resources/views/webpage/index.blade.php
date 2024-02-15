1d0db871586e60f0504b080aa03a6ca26536ed5f
beni07if <beni07elreal@gmail.com>
2024-02-16 01:56:03 +0700

    change carousel image

--- resources/views/webpage/index.blade.php
+++ resources/views/webpage/index.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="hero d-flex align-items-center">
+  <!-- <section id="hero" class="hero d-flex align-items-center">
 
     <div class="container">
       <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
           <h1 data-aos="fade-up">The world's leading services for sustainable supply chain</h1>
           <h2 data-aos="fade-up" data-aos-delay="400">Comprehensive cutting-edge solutions created to achieve sustainability in your supply chain. Powered by Inovasi Digital</h2>
-          <!-- <div data-aos="fade-up" data-aos-delay="600">
+          <div data-aos="fade-up" data-aos-delay="600">
             <div class="text-center text-lg-start">
               <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                 <span>Get Started</span>
                 <i class="bi bi-arrow-right"></i>
               </a>
             </div>
-          </div> -->
+          </div>
         </div>
         <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
           <img src="bgwp.img" class="img-fluid" alt="">
@@ -25,7 +25,14 @@
       </div>
     </div>
 
-  </section><!-- End Hero -->
+  </section> -->
+  <section class="breadcrumbs bg-transparent" style="background-image: url('template/flexstart/assets/img/bg-carousel.png'); background-size: contain; background-position: center; height: 80vh; width: 100%; display: flex; justify-content: center; align-items: center;">
+    <div class="container">
+        <!-- Konten di sini -->
+    </div>
+</section>
+
+  <!-- End Hero ss-->
 
   <main id="main">
     <!-- ======= About Section ======= -->
